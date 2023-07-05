f11334101d1cfe827493dd0be1464c814a8f3872
Allen Yeh <allen_yeh@systemweb.com.tw>
2023-07-05 10:00:50 +0800

    📙chapter: 打包行李

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@
 <template>
   <div class="reveal">
     <div class="slides">
+      <!-- <RevealSectionExample></RevealSectionExample> -->
       <Preamble></Preamble>
       <StrartTrip></StrartTrip>
+      <PackYourBags></PackYourBags>
     </div>
   </div>
 </template>
@@ -26,6 +28,7 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.js'
 import { onMounted } from 'vue'
 // chapters
 // import RevealSectionExample from './components/RevealSectionExample.vue'
+import PackYourBags from '@/chapters/packYourBags/index.vue'
 import Preamble from '@/chapters/preamble/index.vue'
 import StrartTrip from '@/chapters/strartTrip/index.vue'
 
